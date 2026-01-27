11c8c2493f90c47c68b0f2e1415aa6288cbbbf94
Jonathon Gardner <jonathag@cisco.com>
2026-01-27 10:58:34 -0500

    chore: add iso test with hard link

--- header.go
+++ header.go
@@ -21,6 +21,8 @@ type ArchiveEntry interface {
 	// The name of the entry
 	PathName() string
 	Symlink() string
+	Hardlink() string
+	IsHardlink() bool
 }
 
 type entryImpl struct {
@@ -28,38 +30,60 @@ type entryImpl struct {
 }
 
 type entryInfo struct {
-	stat *C.struct_stat
-	name string
+	stat  *C.struct_stat
+	entry *C.struct_archive_entry
+	name  string
 }
 
 func (h *entryImpl) Stat() os.FileInfo {
 	info := &entryInfo{}
 	info.stat = C.archive_entry_stat(h.entry)
+	info.entry = h.entry
 	info.name = filepath.Base(h.PathName())
 	return info
 }
 
+// PathName returns the path name of the entry
 func (h *entryImpl) PathName() string {
 	name := C.archive_entry_pathname(h.entry)
 
 	return C.GoString(name)
 }
 
+// Symlink returns the symlink name of the entry
+// returns empty string if no symlink is set
 func (h *entryImpl) Symlink() string {
 	name := C.archive_entry_symlink(h.entry)
 
 	return C.GoString(name)
 }
 
+// Hardlink returns the hardlink name of the entry
+// returns empty string if no hardlink is set
+func (h *entryImpl) Hardlink() string {
+	name := C.archive_entry_hardlink(h.entry)
+
+	return C.GoString(name)
+}
+
+func (h *entryImpl) IsHardlink() bool {
+	return C.archive_entry_hardlink(h.entry) != nil
+}
+
+// Name returns the base name of the entry
 func (e *entryInfo) Name() string {
 	return e.name
 }
 func (e *entryInfo) Size() int64 {
-	return int64(e.stat.st_size)
+	return int64(C.archive_entry_size(e.entry))
 }
 func (e *entryInfo) Mode() os.FileMode {
-	mode := os.FileMode(e.stat.st_mode & 0777)
-	switch e.stat.st_mode & syscall.S_IFMT {
+	// Use archive_entry_perm for permissions
+	mode := os.FileMode(C.archive_entry_perm(e.entry))
+
+	// Use archive_entry_filetype for file type (more reliable for all formats)
+	fileType := C.archive_entry_filetype(e.entry)
+	switch fileType {
 	case syscall.S_IFLNK:
 		mode |= os.ModeSymlink
 	case syscall.S_IFSOCK:
@@ -76,7 +100,8 @@ func (e *entryInfo) Mode() os.FileMode {
 	return mode
 }
 func (e *entryInfo) IsDir() bool {
-	return e.stat.st_mode&syscall.S_IFDIR != 0
+	fileType := C.archive_entry_filetype(e.entry)
+	return fileType == syscall.S_IFDIR
 }
 func (e *entryInfo) Sys() interface{} {
 	return e.stat
